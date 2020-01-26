f9d68578def0bfeedcd4bd774135a0883492d276
alex-i-git <a.izmaylove@gmail.com>
2020-01-26 20:16:17 +0300

    *

--- MinNumber/src/MinNumber.java
+++ MinNumber/src/MinNumber.java
@@ -1,2 +1,27 @@
+
+import java.util.Scanner;
+
 public class MinNumber {
+
+    // 1. Даны три числа. Найти наименьшее из них.
+
+    public static void main(String[] args) {
+
+        double a = 0, b = 0, c = 0, min = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите число a: ");
+        a = sc.nextDouble();
+        System.out.println("Введите число b: ");
+        b = sc.nextDouble();
+        System.out.println("Введите число c: ");
+        c = sc.nextDouble();
+
+        min = (a<b)?(min=a):(min=b);
+        min = (c<min)?(min=c):(min=min+0);
+
+        System.out.println("Наименьшее число " +min);
+
+    }
+
 }
