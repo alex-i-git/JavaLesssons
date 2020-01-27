fc6b5bee4e9aab0e09b6098c113be5c209078669
alex-i-git <a.izmaylove@gmail.com>
2020-01-27 10:07:08 +0300

    *

--- PhoneTP/src/PhoneTP.java
+++ PhoneTP/src/PhoneTP.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PhoneTP {
 
     //Написать программу, вычисляющую стоимость 10 минутного
@@ -6,6 +8,39 @@ public class PhoneTP {
     //Киров(800) - 5.00руб. )
 
     public static void main(String[] args) {
+
+        double time = 10, cost=0;
+        int c = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите код города: ");
+
+        c = sc.nextInt();
+
+        switch (c){
+
+            case 905:
+                cost = time*4.15;
+                break;
+
+            case 194:
+                cost = time*1.98;
+                break;
+
+            case 491:
+                cost = time*2.69;
+                break;
+
+            case 800:
+                cost = time*5.00;
+                break;
+
+             default:
+
+                 cost = 0;
+        }
+
+        System.out.println(cost);
 
     }
 }
