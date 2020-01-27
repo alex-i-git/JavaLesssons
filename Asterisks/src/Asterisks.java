63212264d32ac6eb77687b3c39c95de8eeaa66a4
alex-i-git <a.izmaylove@gmail.com>
2020-01-27 18:18:37 +0300

    *

---
+++ Asterisks/src/Asterisks.java
@@ -0,0 +1,11 @@
+public class Asterisks {
+
+    // 6. Необходимо написать программу, которая бы вывела в консоль звездочки
+    // вот таким образом, как на картинке ниже.
+
+    public static void main(String[] args) {
+        for(int i = 0;i<3;i++) {
+            System.out.println("* * * * *");
+        }
+    }
+}
