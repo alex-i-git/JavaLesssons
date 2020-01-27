7a42125950ab2dda069f19557558aec56534644f
alex-i-git <a.izmaylove@gmail.com>
2020-01-27 12:34:56 +0300

    *

---
+++ From5To1/src/From5To1.java
@@ -0,0 +1,12 @@
+public class From5To1 {
+    //2. Необходимо вывести на экран числа от 5 до 1.
+
+    public static void main(String[] args) {
+        for(int i = 5;i>0;i--){
+            System.out.println(i);
+
+        }
+    }
+
+}
+
