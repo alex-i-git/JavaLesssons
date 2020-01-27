afd1510086a7f58bc46c431b43744d78b3ce6ca7
alex-i-git <a.izmaylove@gmail.com>
2020-01-27 12:45:16 +0300

    *

---
+++ Sum1To10/src/Sum1To10.java
@@ -0,0 +1,14 @@
+public class Sum1To10 {
+
+    // Просуммировать все числа от 1 до 10 и вывести на экран
+
+    public static void main(String[] args) {
+
+        int s = 0;
+        for(int i = 1;i<=10;i++){
+
+            s = s + i;
+        }
+        System.out.println(s);
+    }
+}
